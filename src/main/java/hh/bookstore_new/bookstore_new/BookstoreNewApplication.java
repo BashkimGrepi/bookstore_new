f4ec71634c15f60c4419b2007771e73dfd421e5b
BashkimGrepi <bashkim.grepi@gmail.com>
2025-02-09 15:59:05 +0200

    Added JPA and H2 database support with simple data

--- src/main/java/hh/bookstore_new/bookstore_new/BookstoreNewApplication.java
+++ src/main/java/hh/bookstore_new/bookstore_new/BookstoreNewApplication.java
@@ -1,13 +1,27 @@
 package hh.bookstore_new.bookstore_new;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.bookstore_new.bookstore_new.domain.Book;
+import hh.bookstore_new.bookstore_new.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreNewApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreNewApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner demo(BookRepository bookRepository) {
+		return (args) -> {
+			bookRepository.save(new Book("Odyssean ihmeet", "James Dean", 1996, 6453, 49.99));
+			bookRepository.save(new Book("Harry potter", "J.K.Rowling", 1996, 6453, 49.99));
+			bookRepository.save(new Book("Odyssean ihmeet", "James Dean", 1996, 6453, 49.99));
+		
+		};
 	}
 
 }
---
+++ src/main/java/hh/bookstore_new/bookstore_new/domain/BookRepository.java
@@ -0,0 +1,7 @@
+package hh.bookstore_new.bookstore_new.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+}
