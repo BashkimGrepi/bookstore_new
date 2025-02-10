c338405b0a1a97b6b2e83c2f5b8bad12cccbb6df
BashkimGrepi <bashkim.grepi@gmail.com>
2025-02-10 09:53:18 +0200

    C3.4

--- src/main/java/hh/bookstore_new/bookstore_new/web/BookController.java
+++ src/main/java/hh/bookstore_new/bookstore_new/web/BookController.java
@@ -6,6 +6,9 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import hh.bookstore_new.bookstore_new.domain.Book;
 import hh.bookstore_new.bookstore_new.domain.BookRepository;
@@ -27,5 +30,22 @@ public class BookController {
         books = (List<Book>) bookRepository.findAll();
         model.addAttribute("books", books);
         return "booklist";
+    }
+
+    @GetMapping("/addbook")
+    public String showAddBookForm(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+      @PostMapping("/addbook")
+      public String addBook(@ModelAttribute Book book) {
+          bookRepository.save(book);
+          return "redirect:/booklist";
+      }
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+        return "redirect:/booklist";
     }
 }
