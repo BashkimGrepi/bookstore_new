8f3491db1c590e6848c4d8e8f5850c913a983dc7
BashkimGrepi <bashkim.grepi@gmail.com>
2025-02-10 09:43:55 +0200

    viikko 4 tehtävät

--- src/main/java/hh/bookstore_new/bookstore_new/web/BookController.java
+++ src/main/java/hh/bookstore_new/bookstore_new/web/BookController.java
@@ -1,16 +1,31 @@
 package hh.bookstore_new.bookstore_new.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import hh.bookstore_new.bookstore_new.domain.Book;
+import hh.bookstore_new.bookstore_new.domain.BookRepository;
 
 @Controller
 public class BookController {
 
-    
-     //http://localhost:8080/index
-    @GetMapping("/index")
-    public String index() {
-        
-        return "";
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    List<Book> books = new ArrayList<Book>();
+
+    // http://localhost:8080/booklist
+    @GetMapping("/booklist")
+    public String showAllBooks(Model model) {
+        books = (List<Book>) bookRepository.findAll();
+        model.addAttribute("books", books);
+        return "booklist";
     }
 }
